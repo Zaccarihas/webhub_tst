c6a9e33e109a0c9d616fe990adbd67307b5de3cf
Anders Löfqvist <lofqvist.anders@gmail.com>
2023-04-08 17:13:39 +0200

    Refactored and OOP implemented

--- _todo.md
+++ _todo.md
@@ -7,19 +7,19 @@ To Do
 
 In progress
 ---------------------------------------------------------------------------------------------------
-* Fixa auktoriseringsproblemen för GIT på html/nav
-* Sätt upp en ny GIT för secure
-* Gör om till OO och läs på om autoloaders. (jmf med den autoloader som composer redan skapat i vendor. - Kan man ha flera autoloader eller måste jag lägga mina egna klasser under vendor. Kan jag modifiera autoloader under vendor så jag kan lägga mina egna klasser någon annanstans eller måste jag registrera mina klasser enlig paketen på packagist?)
+* Gör om till OOP och läs på om autoloaders. (jmf med den autoloader som composer redan skapat i vendor. - Kan man ha flera autoloader eller måste jag lägga mina egna klasser under vendor. Kan jag modifiera autoloader under vendor så jag kan lägga mina egna klasser någon annanstans eller måste jag registrera mina klasser enlig paketen på packagist?)
+    * Gör en klass för authorization.php
 * Lägg över de specifika sidkontrollerna för dynamiska sidor till secure
 * Flytta innehållsmappen och config-mappen till websecure
 
 
 Backlog
 ---------------------------------------------------------------------------------------------------
+* Fixa sidonavigeringen. Det måste vara möjligt att återgå till mappens index.md samt att hoppa upp till överställd mapps index.md
 * Gör en named session istället för en unnamed.
 * Fixa till styling för sidebar
-* Skapa en RESTful router
-* Sätt up en adminsida där man kan godkänna användare (active), ändar användaruppgifter, ta bort användare  och ändra lösenord.
+* Skapa en RESTful router (jmf rewritetest-mappen på local)
+* Sätt up en adminsida där man kan godkänna användare (active), ändra användaruppgifter, ta bort användare  och ändra lösenord.
 * Skapa en online md-editor
 * Läs på mer om Twig envirenment och hur man använder cache
 ~~~
@@ -30,7 +30,7 @@ $twig = new \Twig\Environment($loader, [
 * Ändra storlek på ikonen 
 * Gör om databaskopplingen så den använder pdo-rutiner istället för mysqli (authentication jmf med signup.php)
 * Presentera anropande ip-nummer
-* Fixa sidonavigeringen. Det måste vara möjligt att återgå till mappens index.md samt att hoppa upp till överställd mapps index.md
+* Gör en logg för varje användare över vilka ip de brukar ansluta från. Låt användaren bestämma om endast betrodda ip-nummer får användas för uppkoppling eller om användaren vill ha en varning med möjlighet till utloggning och spärr om inloggning sker från okänt ip-nummer.
 * Se serien Udamy Clone på youtube
 * Lyft över stylesheet-genereringen och npm till secure, men se till att resultatfilen hamnar under public så den kan laddas ner av webläsaren
 * Gör ett helt nytt tema (mer modernt - utan klassiskt sidhuvud och sidfot) och testa att växla mellan de olika temorna
@@ -41,7 +41,12 @@ $twig = new \Twig\Environment($loader, [
 * Fixa till en lösning för att presentera felmeddelanden från exempelvis en misslyckad registrering av användare (se signup.php)
     * Fixa till så att en misslyckad inloggning hamnar tillbaka på framsidan med ett felmeddelande.
     * Jämför flash-messages på nätet
+* Fixa stylesheet så att kodrutor håller sig inom överställt objekt
 * Göra nya sidan publik på webservern och ersätta Pico-varianten.
+* Gör en anpassad sida för mobil
+* Gör en anpassad sida för bredare skärmar som tar tillvara mer av ytan.
+* Skapa en blogg funktion
+* Skapa ett forum med PHP-forum
 * Lägg in dbit
 * Lägg in inventory
 * Lägg in home maintenance
@@ -57,24 +62,34 @@ $twig = new \Twig\Environment($loader, [
 * Studera responsiv design via <https://web.dev/learn/design/>
 * Läs och studera boken A beutiful webdesin som finns på nasen.
 * Studera mer om grid layout för websidor
+* Studera kryptering. vad är skillnaden mellan rsa, escd, etc. Hur fungerar ssh och keychain
+* I ParseExtra i vendor-mappen används en utgången funktion mb_convert_encoding. Här: <https://stackoverflow.com/questions/11974008/alternative-to-mb-convert-encoding-with-html-entities-charset> beskrivs ett alternativt sätt men jag vet inte om jag vill uppdatera filer som jag installerat via composer `(htmlspecialchars_decode(utf8_decode(htmlentities($string, ENT_COMPAT, 'utf-8', false)));)`
+* Testa drag and drop funktion på någon sida (ev en kanban-sida för uppgifter)
 
 
 Done
 ---------------------------------------------------------------------------------------------------
+### 2023-04-08
+* Gör om till OOP och läs på om autoloaders. (jmf med den autoloader som composer redan skapat i vendor. - Kan man ha flera autoloader eller måste jag lägga mina egna klasser under vendor. Kan jag modifiera autoloader under vendor så jag kan lägga mina egna klasser någon annanstans eller måste jag registrera mina klasser enlig paketen på packagist?)
+    * Gör en abstrakt klass navigation som innehåller get_folder och låt cls_navbar ärva den abstrakta klassen.
+    * Gör en cls_sidebar som också ärver navigation
+
+### 2023-04-01
+* Sätt upp en ny GIT för secure
+* Fixa auktoriseringsproblemen för GIT på html/nav
 * Flytta innehåll från nuvarande Pico-sida till den nya sidan.
 
+### Tidigare
 * Lägg till inloggning och auktorisering
     * Fixa inloggningsformulär
     * Skapa registration form
     * Begränsa tillgång till content via index.php
         * Begränsa till dynamiska sidor
         * Begränsa till statiska sidor där required roles tas från yaml-delen av sidan.
-
 * Refactoring
     * Beskriv en bra struktur för content, assetts, views, models, themes, etc
     * Studera och implementera bättre lösning för sökvägar (jmf `__DIR__` etc)
     * Gå igenom index.php och stycka upp den i lämpliga funktioner och controllers
     * Gå igenom alla controllers under config-mappen och lägg dem i ny mapp controllers så att config blir ren config (yaml).
-
 * Skapa ett snyggt sidhuvud
     * Jämför med sidhuvudet på webhub men få det lite striktare och mindre likt ett amerikanskt collage lag
--- index.php
+++ index.php
@@ -1,94 +1,100 @@
 <?php 
     
     // Configurate
-    $public_folder = __DIR__;
     require_once('/var/www/secure/nav/controllers/init.php');
+    require_once('/var/www/secure/nav/controllers/cls_site.php');
+    $curpage = htmlentities($_GET['page'] ?? 'index.md');
+    $hub = new Site(__DIR__, $curpage);
+
+    // Rout and render requested page
+    //$hub->current_page = htmlentities($_GET['page']) ?? 'index.md';
+    $hub->route($curpage);
     
     // Set up Twig template generator
-    $loader = new \Twig\Loader\FilesystemLoader($CONFIG['themes_folder'].$CONFIG['Theme']);
-    $twig = new \Twig\Environment($loader, []);
+    // $loader = new \Twig\Loader\FilesystemLoader($CONFIG['themes_folder'].$CONFIG['Theme']);
+    // $twig = new \Twig\Environment($loader, []);
 
     // Controllers (exchange with autoloader)
-    require_once($CONFIG['controllers_folder'].'transform.php');
-    require_once($CONFIG['controllers_folder'].'navigation.php');
-    require_once($CONFIG['controllers_folder'].'authorization.php');
-
+    // require_once($CONFIG['controllers_folder'].'transform.php');
+    // require_once($CONFIG['controllers_folder'].'navigation.php');
+    // require_once($CONFIG['controllers_folder'].'authorization.php');
     
     // Check login status
-    $logged_in = isset($_SESSION['loggedin']);
-    $username = $logged_in?$_SESSION['name']:"Not Logged In";   
+    // $logged_in = isset($_SESSION['loggedin']);
+    // $username = $logged_in ? $_SESSION['name'] : "Not Logged In";   
 
     // Include meta data regarding the page
-    include($CONFIG['controllers_folder']).'pagemeta.php';
+    // include($CONFIG['controllers_folder']).'pagemeta.php';
 
     // Router
-    $attributes = [
-        'loggedin'   => $logged_in,
-        'username'   => $username,
-        'authpath'   => 'config/login.php',
-        'regform'    => 'index.php?page=registration',
-        'loadtime'   => $loadtime,
-        'files'      => $numfiles,
-        'mem'        => $mem,
-        'stylesheet' => 'themes/'.$CONFIG['Theme'].'/style.css',
-        'selected'   => $CONFIG['current_page'],
-    ];
+    // $attributes = [
+    //     'loggedin'   => $logged_in,
+    //     'username'   => $username,
+    //     'authpath'   => 'config/login.php',
+    //     'regform'    => 'index.php?page=registration',
+    //     'loadtime'   => $loadtime,
+    //     'files'      => $numfiles,
+    //     'mem'        => $mem,
+    //     'stylesheet' => 'themes/'.$CONFIG['Theme'].'/style.css',
+    //     'selected'   => $CONFIG['current_page'],
+    // ];
+
     
-    switch($CONFIG['current_page']) {
-        case 'registration':
+    // switch($CONFIG['current_page']) {
+    //     case 'registration':
 
-            // The registration page is open for all users - no need for authorization
+    //         // The registration page is open for all users - no need for authorization
 
-            $template = 'registration.twig';
-            $attributes['title'] = 'Registration';
-            $attributes['regpath'] = 'config/signup.php';
-            break;
+    //         $template = 'registration.twig';
+    //         $attributes['title'] = 'Registration';
+    //         $attributes['regpath'] = 'config/signup.php';
+    //         break;
 
-        case 'admin':
+    //     case 'admin':
             
-            // Set required role for this page
-            $required_role = 'admin'; // Only users with the admin role can access the page
+    //         // Set required role for this page
+    //         $required_role = 'admin'; // Only users with the admin role can access the page
             
-            // Check if user has the required role
-            if(authorize_user($username, $required_role)) {
-                $template = 'admin.twig';
-                $attributes['title'] = 'Admin sidan';
-            } else {
-                header('Location: http://'.$_SESSION['site_url']);
-            }
+    //         // Check if user has the required role
+    //         if(authorize_user($username, $required_role)) {
+    //             $template = 'admin.twig';
+    //             $attributes['title'] = 'Admin sidan';
+    //         } else {
+    //             header('Location: http://'.$_SESSION['site_url']);
+    //         }
 
-            break;
+    //         break;
 
-        default:
+    //     default:
 
-            $filename = $CONFIG['content_folder'].$CONFIG['current_page'];
-            $file = file($filename);     // Read the file
-            $yaml = extract_yaml($file); // Separate yaml from content
-            $page = implode($file);      // Transform file content into a string
+    //         $filename = $CONFIG['content_folder'].$CONFIG['current_page'];
+    //         $file = file($filename);     // Read the file
+    //         $yaml = extract_yaml($file); // Separate yaml from content
+    //         $page = implode($file);      // Transform file content into a string
                 
-            // Set the required role for the page according to the yaml-settings.
-            $required_role = $yaml['Role'] ?? '';
+    //         // Set the required role for the page according to the yaml-settings.
+    //         $required_role = $yaml['Role'] ?? '';
 
-            // Check if user has the required role
-            $authorized = true;
-            if ($required_role != ''){
-                $authorized = isset($_SESSION['loggedin']) && authorize_user($username, $required_role);
-            }
+    //         // Check if user has the required role
+    //         $authorized = true;
+    //         if ($required_role != ''){
+    //             $authorized = isset($_SESSION['loggedin']) && authorize_user($username, $required_role);
+    //         }
 
-            if ($authorized) {
-                // Generate content from parsedown-file
-                $parsedown = new ParsedownExtra();
-                $content = $parsedown->text($page);
+    //         if ($authorized) {
+    //             // Generate content from parsedown-file
+    //             $parsedown = new ParsedownExtra();
+    //             $content = $parsedown->text($page);
                 
-                $template = 'index.twig';
-                $attributes['title'] = $yaml["Title"] ?? 'Unset';
-                $attributes['nav'] = $nav;
-                $attributes['side_nav'] = $side_nav;
-                $attributes['content'] = $content;
-            } else {
-                $template = 'unauthorized.twig';
-                $attributes['title'] = "Ej tillgänglig";
-            }
-    }
+    //             $template = 'index.twig';
+    //             $attributes['title'] = $yaml["Title"] ?? 'Unset';
+    //             $attributes['nav'] = $nav;
+    //             $attributes['side_nav'] = $side_nav;
+    //             $attributes['content'] = $content;
+    //         } else {
+    //             $template = 'unauthorized.twig';
+    //             $attributes['title'] = "Ej tillgänglig";
+    //         }
+    // }
 
-    echo $twig->render($template, $attributes);
+    // echo $twig->render($template, $attributes);
